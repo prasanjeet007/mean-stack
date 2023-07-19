f36619c5ab7f2d974d518206d9deead9609856a0
Prasanjeet Chatterjee <prasanjeetchatterjee28@gmail.com>
2023-07-19 20:01:02 +0530

    add connection file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = 3000;
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server is running on the ${process.env.PORT || PORT} port`);
+const port = process.env.PORT || 3000;
+app.set("port", port);
+app.use(cors());
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`Server is running on the ${port} port`);
 });
